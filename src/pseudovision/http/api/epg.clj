54d311a896dd135b1f71985a2822b00bb4fc9ff7
niten <niten@fudo.org>
2026-03-27 12:41:31 -0700

    Update deps

--- src/pseudovision/http/api/epg.clj
+++ src/pseudovision/http/api/epg.clj
@@ -2,32 +2,33 @@
   "XMLTV EPG endpoint (/xmltv or /epg.xml).
    Returns an XMLTV-formatted XML document covering the next 7 days."
   (:require [pseudovision.db.playouts :as db]
-            [pseudovision.util.time   :as t])
+            [pseudovision.util.time   :as t]
+            [clojure.string :as str])
   (:import [java.time Instant]))
 
 (defn- channel->xmltv
   "Renders a channel map as an XMLTV <channel> element string."
   [{:keys [channels/uuid channels/name channels/number
-                                channels/group-name] :as ch}]
+           channels/group-name] :as ch}]
   (str "<channel id=\"" uuid "\">"
-       "<display-name>" (clojure.string/replace name #"[<>&]" "") "</display-name>"
+       "<display-name>" (str/replace name #"[<>&]" "") "</display-name>"
        "<lcn>" number "</lcn>"
        (when group-name
-         (str "<group>" (clojure.string/replace group-name #"[<>&]" "") "</group>"))
+         (str "<group>" (str/replace group-name #"[<>&]" "") "</group>"))
        "</channel>"))
 
 (defn- event->xmltv
   "Renders a playout event as an XMLTV <programme> element string."
   [{:keys [playout-events/start-at playout-events/finish-at
-                               playout-events/custom-title channels/uuid
-                               metadata/title metadata/plot]}]
+           playout-events/custom-title channels/uuid
+           metadata/title metadata/plot]}]
   (let [display-title (or custom-title title "Unknown")]
     (str "<programme start=\"" (t/->xmltv-date start-at)
          "\" stop=\""          (t/->xmltv-date finish-at)
          "\" channel=\""       uuid "\">"
-         "<title lang=\"en\">" (clojure.string/replace display-title #"[<>&]" "") "</title>"
+         "<title lang=\"en\">" (str/replace display-title #"[<>&]" "") "</title>"
          (when plot
-           (str "<desc lang=\"en\">" (clojure.string/replace plot #"[<>&]" "") "</desc>"))
+           (str "<desc lang=\"en\">" (str/replace plot #"[<>&]" "") "</desc>"))
          "</programme>")))
 
 (defn xmltv-handler
@@ -41,13 +42,13 @@
           ;; Deduplicate channels preserving insertion order
           channels (->> events
                         (map #(select-keys % [:channels/uuid :channels/name
-                                               :channels/number :channels/group-name]))
+                                              :channels/number :channels/group-name]))
                         (distinct))]
       {:status  200
        :headers {"Content-Type" "text/xml; charset=utf-8"}
        :body    (str "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
                      "<!DOCTYPE tv SYSTEM \"xmltv.dtd\">"
                      "<tv generator-info-name=\"pseudovision\">"
-                     (clojure.string/join (map channel->xmltv channels))
-                     (clojure.string/join (map event->xmltv   events))
+                     (str/join (map channel->xmltv channels))
+                     (str/join (map event->xmltv   events))
                      "</tv>")})))
