372d82a40af55409e126423eefa42d1cf6b70d48
niten <niten@fudo.org>
2026-04-05 18:29:19 -0700

    Add SQL logging to debug ON CONFLICT clause generation

--- src/pseudovision/db/media.clj
+++ src/pseudovision/db/media.clj
@@ -136,18 +136,20 @@
   (let [prepared (cond-> attrs
                    (:kind attrs)  (update :kind #(sql-util/->pg-enum "media_item_kind" %))
                    (:state attrs) (update :state #(sql-util/->pg-enum "media_item_state" %)))
-        result   (db/execute-one! ds (-> (h/insert-into :media-items)
-                                         (h/values [prepared])
-                                         (h/on-conflict :library-path-id :remote-key)
-                                         (h/do-update-set :state :remote-etag :position)
-                                         sql/format))]
-    (log/info "Upserted media item"
-              {:media-item-id   (:media-items/id result)
-               :kind            (:kind attrs)
-               :library-path-id (:library-path-id attrs)
-               :remote-key      (:remote-key attrs)
-               :parent-id       (:parent-id attrs)})
-    result))
+        sql-vec  (-> (h/insert-into :media-items)
+                     (h/values [prepared])
+                     (h/on-conflict :library-path-id :remote-key)
+                     (h/do-update-set :state :remote-etag :position)
+                     sql/format)]
+    (log/info "Media item upsert SQL" {:sql (first sql-vec)})
+    (let [result (db/execute-one! ds sql-vec)]
+      (log/info "Upserted media item"
+                {:media-item-id   (:media-items/id result)
+                 :kind            (:kind attrs)
+                 :library-path-id (:library-path-id attrs)
+                 :remote-key      (:remote-key attrs)
+                 :parent-id       (:parent-id attrs)})
+      result)))
 
 ;; ---------------------------------------------------------------------------
 ;; Collections
