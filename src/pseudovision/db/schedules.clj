e295118199969007e4aad72d4f254fee8f58fd86
niten <niten@fudo.org>
2026-04-12 22:55:32 -0700

    Add debug logging to slot creation to diagnose schedule_id issue

--- src/pseudovision/db/schedules.clj
+++ src/pseudovision/db/schedules.clj
@@ -2,7 +2,8 @@
   (:require [honey.sql         :as sql]
             [honey.sql.helpers :as h]
             [pseudovision.db.core :as db]
-            [pseudovision.util.sql :as sql-util]))
+            [pseudovision.util.sql :as sql-util]
+            [taoensso.timbre :as log]))
 
 ;; ---------------------------------------------------------------------------
 ;; Schedules
@@ -54,19 +55,24 @@
                        sql/format)))
 
 (defn create-slot! [ds attrs]
-  (db/execute-one! ds (-> (h/insert-into :schedule-slots)
-                          (h/values [(cond-> attrs
-                                       (:anchor attrs)
-                                       (update :anchor #(sql-util/->pg-enum "slot_anchor" %))
-                                       (:fill_mode attrs)
-                                       (update :fill_mode #(sql-util/->pg-enum "slot_fill_mode" %))
-                                       (:playback_order attrs)
-                                       (update :playback_order #(sql-util/->pg-enum "playback_order" %))
-                                       (:block_duration attrs)
-                                       (update :block_duration sql-util/->pg-interval)
-                                       (:start_time attrs)
-                                       (update :start_time sql-util/->pg-interval))])
-                          sql/format)))
+  (let [processed (cond-> attrs
+                    (or (:anchor attrs) (:anchor attrs))
+                    (update :anchor #(sql-util/->pg-enum "slot_anchor" %))
+                    (or (:fill_mode attrs) (:fill-mode attrs))
+                    (update :fill-mode #(sql-util/->pg-enum "slot_fill_mode" %))
+                    (or (:playback_order attrs) (:playback-order attrs))
+                    (update :playback-order #(sql-util/->pg-enum "playback_order" %))
+                    (or (:block_duration attrs) (:block-duration attrs))
+                    (update :block-duration sql-util/->pg-interval)
+                    (or (:start_time attrs) (:start-time attrs))
+                    (update :start-time sql-util/->pg-interval)
+                    (or (:guide_mode attrs) (:guide-mode attrs))
+                    (update :guide-mode #(sql-util/->pg-enum "guide_mode" %)))
+        sql-map (-> (h/insert-into :schedule-slots)
+                    (h/values [processed])
+                    sql/format)]
+    (log/debug "Creating slot" {:attrs attrs :processed processed :sql sql-map})
+    (db/execute-one! ds sql-map)))
 
 (defn update-slot! [ds id attrs]
   (db/execute-one! ds (-> (h/update :schedule-slots)
