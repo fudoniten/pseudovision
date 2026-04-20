d3feccfd13e6638132ebb19517e8887104f07645
niten <niten@fudo.org>
2026-04-19 20:54:08 -0700

    Add debug logging to UUID query in channels handler
    Debug why UUID filter returns all channels instead of filtering.

--- src/pseudovision/http/api/channels.clj
+++ src/pseudovision/http/api/channels.clj
@@ -1,14 +1,21 @@
 (ns pseudovision.http.api.channels
-  (:require [pseudovision.db.channels :as db]))
+  (:require [pseudovision.db.channels :as db]
+            [taoensso.timbre :as log]))
 
 (defn list-channels-handler [{:keys [db]}]
   (fn [req]
     ;; Support ?uuid=xxx query parameter to filter by UUID
-    (let [uuid-param (get-in req [:query-params "uuid"])]
+    (let [uuid-param (get-in req [:query-params "uuid"])
+          _          (when uuid-param
+                       (log/debug "UUID query param received" {:uuid uuid-param}))]
       (if uuid-param
         (if-let [channel (db/get-channel-by-uuid db uuid-param)]
-          {:status 200 :body channel}
-          {:status 404 :body {:error "Channel not found"}})
+          (do
+            (log/debug "Found channel by UUID" {:channel-id (:channels/id channel)})
+            {:status 200 :body channel})
+          (do
+            (log/warn "Channel not found by UUID" {:uuid uuid-param})
+            {:status 404 :body {:error "Channel not found"}}))
         {:status 200 :body (db/list-channels db)}))))
 
 (defn get-channel-handler [{:keys [db]}]
